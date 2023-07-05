37b6bfd3b8f18252567bc74d304bc9ee922bd8a2
bowen <834872527@qq.com>
2023-07-05 08:58:55 +0800

    init repo

---
+++ src/Test01.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author：bowen
+ * @Date：2023/7/4 20:21
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+    }
+
+}
